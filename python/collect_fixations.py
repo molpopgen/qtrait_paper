cc535393194584dacbf14f67ad3858fda1c6be39
molpopgen <krthornt@uci.edu>
2016-04-04 17:32:10 -0700

    new file

---
+++ python/collect_fixations.py
@@ -0,0 +1,21 @@
+import glob
+import pandas as pd
+import feather
+
+ifiles=glob.glob('*.fixations.h5')
+
+data = [pd.read_hdf(i) for i in ifiles]
+
+for i in range(len(ifiles)):
+    fi=ifiles[i].split('_')
+#1,3,5
+    mu=float("0."+str(fi[5].split('.')[1]))
+    data[i]['H2']=[fi[1]]*len(data[i].index)
+    data[i]['opt']=[fi[3]]*len(data[i].index)
+    data[i]['mu']=[mu]*len(data[i].index)
+
+data=pd.concat(data)
+
+feather.write_dataframe(data,'fixations.feather')
+    
+
