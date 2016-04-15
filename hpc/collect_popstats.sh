440bcae2d276c4f6cda857d9dd24dfc2a50c84bf
molpopgen <krthornt@uci.edu>
2016-04-15 12:51:24 -0700

    new file

---
+++ hpc/collect_popstats.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#$ -q krt,krti
+#Not sure how much RAM this needs.
+#$ -pe openmp 64  
+
+cd $SGE_O_WORKDIR
+
+module load krthornt/thorntonlab
+
+/usr/bin/time -f "%e %M" -o collect_popstats.time.txt python python/collect_popstats.py
