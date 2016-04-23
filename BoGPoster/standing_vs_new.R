9acc8fc9e2d55ef1ab8d13e282cc6f98ea4fed5d
Kevin R. Thornton <krthornt@uci.edu>
2016-04-23 16:29:13 -0700

    learned a lesson in being too quick w/dplyr

--- BoGPoster/standing_vs_new.R
+++ BoGPoster/standing_vs_new.R
@@ -3,10 +3,6 @@ library(dplyr)
 
 x=read_feather("standing_vs_new.feather")
 
-means = x %>% group_by(opt,rep,type) %>% mutate(nt=length(type)) %>% group_by(opt,type) %>% summarise(mnt = mean(nt),mft=mean(flen),mesize=mean(esize),mfo=mean(f0))
+meanTypes = x %>% group_by(opt,rep,type) %>% mutate(nt=length(type)) %>% group_by(rep,nt,opt,type) %>% summarise(foo=length(nt)) %>% group_by(opt,type) %>% summarise(mtype=mean(foo),mintype=min(foo),maxtype=max(foo))
 
-print(means)
-
-medians = x %>% group_by(opt,rep,type) %>% mutate(nt=length(type)) %>% group_by(opt,type) %>% summarise(mnt = median(nt),mft=median(flen),mesize=median(esize),mfo=median(f0))
-
-print(medians)
+print(meanTypes)
