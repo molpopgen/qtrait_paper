227ed17473944b72b13ffe57c391195e59195b7c
molpopgen <krthornt@uci.edu>
2016-06-01 08:58:31 -0700

    H^2 = 1.0

--- hpc/sub_samples_time_additive.sh
+++ hpc/sub_samples_time_additive.sh
@@ -1,9 +1,9 @@
 #!sh
 
 #0.1 deleted from both loops right now..
-for H2 in 0.1 0.2 0.5
+for H2 in 1.0
 do
-    for OPT in 0.1 0.5 1 5
+    for OPT in 0.1 0.5 1
     do
 	for mu in 0.00025 0.005 0.001
 	do
