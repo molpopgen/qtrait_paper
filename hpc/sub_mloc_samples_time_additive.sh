f4ef4c99d472547c4b2f396db75abff273c6aded
molpopgen <krthornt@uci.edu>
2016-06-02 09:49:20 -0700

    fix seeding of mloc sample scripts

--- hpc/sub_mloc_samples_time_additive.sh
+++ hpc/sub_mloc_samples_time_additive.sh
@@ -1,12 +1,17 @@
 #!sh
-
+ISEED=202
+I=1
 for H2 in 1.0
 do
     for OPT in 0.1 0.5 1
     do
 	for mu in 0.00025 0.005 0.001
 	do
-	    qsub hpc/mloc_samples_time_additive.sh $mu $H2 H2_"$H2"_OPT_"$OPT"_mu_"$mu".mloc_samples.h5 $OPT 10
+	    SEED=`echo "$ISEED*$I"|bc -l`
+	    echo $SEED
+	    I=$(($I+1))
+	    echo $I
+	    qsub hpc/mloc_samples_time_additive.sh $mu $H2 H2_"$H2"_OPT_"$OPT"_mu_"$mu".mloc_samples.h5 $OPT 10 $SEED
 	done
     done
 done
