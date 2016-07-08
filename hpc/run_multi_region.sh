557116cd69bc3e8e7a0e36e015688ade41aef409
Kevin R. Thornton <krthornt@uci.edu>
2016-07-08 16:01:38 -0700

    new script

---
+++ hpc/run_multi_region.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#$ -q krt,krti,krti,bio,pub64,sf
+#$ -pe openmp 64
+#$ -R y
+
+module load krthornt/thorntonlab
+
+cd $SGE_O_WORKDIR
+sampler=$1
+mutrate=$2
+ofile=$3
+tsample=$4
+optimum=$5
+VS=$6
+sigmu=$7
+seed=$8
+#This will default to 10 loci, 64 cores, 16 batches
+python python/multi_region.py --sampler $sampler -m $mutrate -F $ofile -t $tsample -O $optimum -S $VS -e $sigmu -N 10000 -s $seed
