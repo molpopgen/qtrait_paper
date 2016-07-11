b9e09e87987e15a1b852084f86aa810b919d0686
Kevin R. Thornton <krthornt@uci.edu>
2016-07-11 15:11:58 -0700

    skip lower-mem queues

--- hpc/sub_mloc_summstats_time_additive.sh
+++ hpc/sub_mloc_summstats_time_additive.sh
@@ -13,7 +13,8 @@ do
 	    echo $I
 	    ofile=H2_"$H2"_OPT_"$OPT"_mu_"$mu"_sigmu0.25_10regions_popgen.h5
 	    ofile_fix=H2_"$H2"_OPT_"$OPT"_mu_"$mu"_sigmu0.25_10regions_popgen_fixations.h5
-	    qsub hpc/run_multi_region.sh popgen $mu $ofile 100 $OPT 1 0.25 $SEED --fixations $ofile_fix
+	    #Skip sf queue b/c of possible RAM issues...
+	    qsub -q krt,krti,krti,bio,pub64 hpc/run_multi_region.sh popgen $mu $ofile 100 $OPT 1 0.25 $SEED --fixations $ofile_fix
 	    #qsub hpc/mloc_samples_time_additive.sh $mu $H2 H2_"$H2"_OPT_"$OPT"_mu_"$mu".mloc_samples.h5 $OPT 10 $SEED
 	done
     done
