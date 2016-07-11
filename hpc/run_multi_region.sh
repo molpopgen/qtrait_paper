2a00189943508ddd2e684ff32a16f8ca928d0a6a
molpopgen <krthornt@uci.edu>
2016-07-11 15:22:49 -0700

    fix comment line

--- hpc/run_multi_region.sh
+++ hpc/run_multi_region.sh
@@ -17,5 +17,5 @@ sigmu=$7
 seed=$8
 echo "seed = $seed"
 #This will default to 10 loci, 64 cores, 16 batches
-#$9 and $10 should be --fixations filename to record fixation times & other data
+#Args 9 and 10 should be --fixations filename to record fixation times & other data
 /usr/bin/time -f "%e %M" -o multi_region.$sampler.$optimum.$VS.$mutrate.time python python/multi_region.py --sampler $sampler -m $mutrate -F $ofile -t $tsample -O $optimum -S $VS -e $sigmu -N 10000 -s $seed $9 $10
