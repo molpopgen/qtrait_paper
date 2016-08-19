b0c73e287bec14645b37173c1ba323fef281f4a9
molpopgen <krthornt@uci.edu>
2016-08-19 09:38:00 -0700

    no more pub64 (time limits...)

--- hpc/sub_mloc_summstats_time_additive.sh
+++ hpc/sub_mloc_summstats_time_additive.sh
@@ -15,7 +15,7 @@ do
 	    ofile=H2_"$H2"_OPT_"$OPT"_mu_"$mu"_sigmu0.25_10regions_popgen.h5
 	    ofilefix=H2_"$H2"_OPT_"$OPT"_mu_"$mu"_sigmu0.25_10regions_popgen_fixations.h5
 	    #Skip sf queue b/c of possible RAM issues...
-	    qsub -N MPOPGEN -q krt,krti,krti,bio,pub64 hpc/run_multi_region.sh popgen $mu $ofile 100 $OPT 1 0.25 $SEED --fixations $ofilefix $nsam
+	    qsub -N MPOPGEN -q krt,krti,krti,bio hpc/run_multi_region.sh popgen $mu $ofile 100 $OPT 1 0.25 $SEED --fixations $ofilefix $nsam
 	    #qsub hpc/mloc_samples_time_additive.sh $mu $H2 H2_"$H2"_OPT_"$OPT"_mu_"$mu".mloc_samples.h5 $OPT 10 $SEED
 	done
     done
