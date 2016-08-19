4e754bd0d953669f6d10ee4d306ab86b50777d0b
Kevin R. Thornton <krthornt@uci.edu>
2016-08-19 13:07:48 -0700

    update Makefile targets so that -j works.  Image production now gets output to images/

--- python_plots/compress_image.sh
+++ python_plots/compress_image.sh
@@ -3,6 +3,6 @@
 i=$1
 n=`basename $i .tif`
 convert $i -set colorspace RGB -layers flatten -alpha off -compress lzw -depth 8 \
--density 600 -adaptive-resize 4500x2400  $n"_compressed".tif
+-density 600 -adaptive-resize 4500x2400  images/$n"_compressed".tif
 
-convert $n"_compressed".tif $n"_compressed".pdf
+convert images/$n"_compressed".tif images/$n"_compressed".pdf
