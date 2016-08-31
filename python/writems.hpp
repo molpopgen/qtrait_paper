2b1ca05fc8a2c15c89b06734dfa096819e436cfc
Kevin R. Thornton <krthornt@uci.edu>
2016-08-31 15:23:33 -0700

    no more cout

--- python/writems.hpp
+++ python/writems.hpp
@@ -1,14 +1,15 @@
 #ifndef WRITEMS_HPP
 #define WRITEMS_HPP
-
+#include <iostream>
 #include <Sequence/SimData.hpp>
 #include <sstream>
 #include <zlib.h>
 namespace writems {
 void writeSimData(const Sequence::SimData & d, const std::string & filename) {
+	std::cout << filename << '\n';
 	std::ostringstream o;
 	o << d << '\n';
-	gzFile gz = gzopen(filename.c_str(),"wb");
+	gzFile gz = gzopen(filename.c_str(),"ab");
 	gzwrite(gz,o.str().c_str(),o.str().size());
 	gzclose(gz);
 }
