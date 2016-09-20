d7bcbbf84465dda43e08d4b79cc97e709b6fc307
Kevin R. Thornton <krthornt@uci.edu>
2016-09-20 11:05:35 -0700

    update processing code, although we may replace it entirely once we start working on results of ms-format output

--- python_plots/tenLocusPopgenMeans.py
+++ python_plots/tenLocusPopgenMeans.py
@@ -2,51 +2,66 @@ import pandas as pd
 import glob
 from common_functions import *
 import multiprocessing as mp
+import gc
 
-summstat_files=sorted(glob.glob('../*_10regions_popgen.h5'))
-popstat_files=sorted(glob.glob('../*_10regions_stats.h5'))
 def update_totals(args):
-    filei=args[0]
-    Totals=args[1]
-    START=args[2]
-    STOP=args[3]
-    print filei,START,STOP
-    x=pd.read_hdf(filei,where='rep>=START&rep<STOP')
+    filei=args#[0]
+    print "about to read ",filei
+    #Totals=args[1]
+    #START=args[2]
+    #STOP=args[3]
+    #print filei,START,STOP
+    x=pd.read_hdf(filei)#,where='rep>=START&rep<STOP')
     x.reset_index(inplace=True)
     x.drop_duplicates(inplace=True)
-    xm=x.groupby(['generation','variable']).sum().reset_index()
-    if Totals is None:
-        Totals=xm
-    else:
-        temp = Totals.append(xm)
-        tempg=temp.groupby(['generation','variable']).sum().reset_index()
-        Totals=temp
-    return Totals
+    xm=x.groupby(['generation','variable']).mean().reset_index()
+    return xm
+    #if Totals is None:
+    #    Totals=xm
+    #else:
+    #    temp = Totals.append(xm)
+    #    tempg=temp.groupby(['generation','variable']).sum().reset_index()
+    #    Totals=temp
+    #return Totals
 
 def process_file(ifile,params,out):
     print "processing ",ifile
-    START=0
-    STOP=64
-    Totals=None
-    while START < 1024:
-        P=mp.Pool(1)
-        Totals=P.map(update_totals,[(ifile,Totals,START,STOP)])[0]
-        P.close()
-        #x=pd.read_hdf(ifile,where='rep >= START & rep < STOP')
+    P=mp.Pool(1)
+    Totals=P.map(update_totals,[(ifile)])[0]#,Totals,START,STOP)])[0]
+    P.close()
+    for k in params:
+        Totals[k]=[params[k]]*len(Totals.index)
+    del Totals['rep']
+    del Totals['locus']
+    out.append('means',Totals)
+    del Totals
+    gc.collect()
+    #return
+    #START=0
+    #STOP=64
+    #Totals=None
+    #while START < 1024:
+    #    P=mp.Pool(1)
+    #    Totals=P.map(update_totals,[(ifile,Totals,START,STOP)])[0]
+    #    P.close()
+    #    #x=pd.read_hdf(ifile,where='rep >= START & rep < STOP')
         #x.reset_index(inplace=True)
         #x.drop_duplicates(inplace=True)
         #xm=x.groupby(['generation','variable']).mean()
         #xm.reset_index(inplace=True)
-        START+=64
-        STOP+=64
-    for k in params:
-        Totals[k]=[params[k]]*len(Totals.index)
-    Totals.value/=float(10*1024/64)
-    del Totals['rep']
-    del Totals['locus']
-    out.append('means',Totals)
+    #    START+=64
+    #    STOP+=64
+    #for k in params:
+    #    Totals[k]=[params[k]]*len(Totals.index)
+    #Totals.value/=float(10*1024/64)
+    #del Totals['rep']
+    #del Totals['locus']
+    #out.append('means',Totals)
 
 out=pd.HDFStore('tenLocusPopgenMeans.h5','w',complevel=6,complib='zlib')
+
+summstat_files=sorted(glob.glob('../*_10regions_popgen.h5'))
+popstat_files=sorted(glob.glob('../*_10regions_stats.h5'))
 
 for i,j in zip(summstat_files,popstat_files):
     pi = parse_params(i)
