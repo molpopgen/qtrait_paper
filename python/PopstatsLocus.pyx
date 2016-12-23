168721b029e3629f32e5469a5cbcad9e98620e11
molpopgen <krthornt@uci.edu>
2016-12-23 14:19:48 -0800

    use fwdpy version of sum of squares

--- python/PopstatsLocus.pyx
+++ python/PopstatsLocus.pyx
@@ -14,6 +14,11 @@ from cython_gsl.gsl_math cimport *
 from cython_gsl.gsl_sort cimport * 
 from fwdpy.fwdpy cimport TemporalSampler,GSLrng,sampler_base,custom_sampler,multilocus_t,uint,sample_sep_single_mloc
 
+from fwdpy.numeric_gsl cimport sum_of_squares 
+
+cdef extern from "gsl/gsl_sys.h":
+    int gsl_isnan(double) nogil
+
 cdef extern from "<algorithm>" namespace "std" nogil:
     void reverse[iter](iter,iter)
     int count[iter,value](iter,iter,value)
@@ -31,35 +36,6 @@ cdef struct statdata:
 ctypedef vector[statdata] final_t
 
 ctypedef custom_sampler[final_t] PopstatsLocus_t
-
-cdef pair[double,vector[double]] sum_of_squares(gsl_vector * v,
-                                                gsl_matrix * m) nogil:
-    cdef gsl_vector_ptr_t TAU,SUMS
-    cdef gsl_matrix_ptr_t Q,R
-    TAU.reset(gsl_vector_alloc(min(m.size1,m.size2)))
-    SUMS.reset(gsl_vector_alloc(m.size1))
-    Q.reset(gsl_matrix_alloc(m.size1,m.size1))
-    R.reset(gsl_matrix_alloc(m.size1,m.size2))
-
-    gsl_linalg_QR_decomp(m,TAU.get())
-    gsl_linalg_QR_unpack(m,TAU.get(),Q.get(),R.get())
-    gsl_blas_dgemv(CblasTrans,1.0,Q.get(),v,0.0,SUMS.get())
-
-    cdef pair[double,vector[double]] rv
-
-    cdef size_t i
-    for i in range(0,m.size2): 
-        rv.second.push_back(gsl_pow_2(gsl_vector_get(SUMS.get(),i+1)))
-   
-    cdef size_t DF = m.size2-1
-    RSS=0.0
-    for i in range(DF+1,SUMS.get().size):
-        RSS += gsl_pow_2(gsl_vector_get(SUMS.get(),i))
-
-    cdef double sqi=0.
-    rv.first = RSS
-    for sqi in rv.second: rv.first += sqi
-    return rv
 
 cdef void popstats_locus_details(const multilocus_t * pop,
         const unsigned generation,
@@ -120,8 +96,6 @@ cdef void popstats_locus_details(const multilocus_t * pop,
                 temp.value=0.0
                 temp.rank=locus+1
                 f.push_back(temp)
-            gsl_matrix_free(LOCI.get())
-            gsl_vector_free(GVALUES.get())
             return 
         #We cannot include these loci in the calculation,
         #and so we'll change size2 (no. columns) to reflect
