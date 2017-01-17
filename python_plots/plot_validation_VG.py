d4b9265d289df8abf62f028149fa959570942a41
molpopgen <krthornt@uci.edu>
2017-01-17 14:24:02 -0800

    tweaks to plotting script

--- python_plots/plot_validation_VG.py
+++ python_plots/plot_validation_VG.py
@@ -6,21 +6,22 @@ import numpy as np
 x=pd.read_hdf('validation_summaries.h5')
 
 sigmu = sorted(x.sigmu.unique())
-sige = sorted(x.sige.unique())
 
 linecolors = [ plt.cm.viridis(f) for f in np.linspace(0.1,0.9,4) ]
-f,ax=plt.subplots(3,2,sharex='col',sharey='row')
+f,ax=plt.subplots(3,sharex='col',sharey='row')
 
-for column in range(len(sige)):
-    for row in range(len(sigmu)):
-        xi=x[(x.sige==sige[column])&(x.sigmu==sigmu[row])]
-        xig=xi.groupby(['r'])
-        ax[row,column].plot(sorted(xi.mu.unique()),sorted(xi.EVG.unique()),ls='dotted',color='black')
-        coloridx=0
-        for n,g in xig:
-            print n
-            ax[row,column].plot(g.mu,g.meanVG,ls='solid',color=linecolors[coloridx])
-            ax[row,column].set_title(r'$\sigma_E = $'+'{0:0.2f}'.format(sige[column]) +r', $\sigma_\mu = $'+'{0:0.2f}'.format(sigmu[row]))
-            coloridx+=1
+for row in range(len(sigmu)):
+    xi=x[(x.sigmu==sigmu[row])]
+    xig=xi.groupby(['r'])
+    ax[row].plot(sorted(xi.mu.unique()),sorted(xi.EVG.unique()),ls='dotted',color='black')
+    coloridx=0
+    for n,g in xig:
+        print n
+        ax[row].plot(g.mu,g.meanVG,ls='solid',color=linecolors[coloridx],label=r'$r = $'+'{0:0.2f}'.format(n))
+        ax[row].set_title(r'$\sigma_\mu = $'+'{0:0.3f}'.format(sigmu[row]))
+        coloridx+=1
+
+for axi in range(len(sigmu)):
+    ax[axi].legend(loc='best',prop={'size':10})
 
 plt.savefig('foo.tiff')
