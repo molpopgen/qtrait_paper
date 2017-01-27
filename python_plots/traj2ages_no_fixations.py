79abe87bf37c8bad099690f33b469ce19f97275c
molpopgen <krthornt@uci.edu>
2017-01-27 08:39:13 -0800

    update trajectories workflow

--- python_plots/traj2ages_no_fixations.py
+++ python_plots/traj2ages_no_fixations.py
@@ -7,8 +7,17 @@ import sys
 def run_query(args):
     dbfile,repid=args
     conn=sqlite3.connect(dbfile)
-    sql='select generation,count(generation) as nobs,avg(generation-origin) as mean_age from freqs where rep == '+str(repid)+' group by generation;'
-    rv= pd.read_sql_query(sql,conn)
+    sql='select pos,esize,max(freq) as mfreq from freqs where rep == '+str(repid)+' group by pos,esize'
+    maxfreqs=pd.read_sql_query(sql,conn)
+    sql='select pos,esize,generation,generation-origin as age from freqs where rep == '+str(repid)+' group by pos,esize,generation;'
+    ages= pd.read_sql_query(sql,conn)
+    maxfreqs.set_index(['pos','esize'],inplace=True)
+    ages.set_index(['pos','esize'],inplace=True)
+    joined=maxfreqs.join([ages],how='left').reset_index()
+    joined=joined[joined.mfreq<1.]
+    rv=joined.groupby(['generation']).agg({'age':{'nobs':'count','mean_age':np.mean}}).reset_index()
+    rv.columns=rv.columns.droplevel(0)
+    rv.columns.values[0]='generation'
     rv['repid']=[repid]*len(rv.index)
     conn.close()
     return rv
@@ -18,7 +27,7 @@ if __name__=="__main__":
     outfile=sys.argv[2]
     P=mp.Pool()
     args=[(dbfile,i) for i in range(1024)]
-    results=P.map(run_query,args,8)
+    results=P.map(run_query,args,4)
     P.close()
     P.join()
     allres=pd.concat([res for res in results])
