3c7a253719bdd5b2a534bb2ed2573f1a931bfcba
molpopgen <krthornt@uci.edu>
2017-02-02 12:05:29 -0800

    add Makefile targets and update python script to handle case sensitivity issues with SQL

--- sqlite/python/todb.py
+++ sqlite/python/todb.py
@@ -8,9 +8,20 @@ if __name__=="__main__":
     infile=sys.argv[1]
     dbname=sys.argv[2]
 
-    x=pd.read_hdf(infile,chunksize=50000,iterator=True)
+    x=pd.read_hdf(infile,chunksize=1000000,iterator=True)
     conn=sqlite3.connect(dbname)
     for chunk in x:
-        chunk.to_sql('data',conn,if_exists='append')
+        chunk.reset_index(inplace=True,drop=True)
+        indexes=[]
+        if 'rep' in chunk:
+            indexes.append('rep')
+        if 'generation' in chunk:
+            indexes.append('generation')
+        haveIndex=False
+        if len(indexes) > 0:
+            chunk.set_index(indexes,inplace=True,drop=True)
+            haveIndex=True
+        chunk.rename(columns={"total_Aa":"total_het","total_aa":"total_hom"},inplace=True)
+        chunk.to_sql('data',conn,if_exists='append',index=haveIndex)
     conn.close()
 
