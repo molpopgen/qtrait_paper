f7fa0d9cbff8e5f71c164893b926f4d29259d31a
molpopgen <krthornt@uci.edu>
2017-05-08 12:34:04 -0700

    updat Makfile

---
+++ sqlite/R/summarise_10locus_popgen.R
@@ -0,0 +1,37 @@
+#!/usr/bin/env r
+
+suppressMessages(library(dplyr))
+suppressMessages(library(tidyr))
+suppressMessages(library(RSQLite))
+suppressMessages(library(docopt))
+source("filename2params.R")
+
+doc <- "Usage: summarise_10locus_popgen.R [-i <infile> -o <outfile>]"
+
+options <- docopt(doc)
+
+db <- src_sqlite(options$infile)
+
+data <- tbl(db,'data')
+
+query <- data %>%
+    #Drop the locus column, as we don't need it
+    select(-locus) %>%
+    filter(generation >= 8*5000) %>%
+    group_by(generation,variable) %>%
+    summarise(mvalue = mean(value))
+
+results = collect(query)
+
+params = getparams(options$infile)
+results.tidy = spread(results,key=variable,value=mvalue) %>%
+	mutate(opt=params$opt) %>%
+	mutate(mu=params$mu)
+
+gzo=gzfile(options$outfile,"w")
+
+write.table(results.tidy,gzo,quote=FALSE,
+           row.names=FALSE)
+
+close(gzo)
+
