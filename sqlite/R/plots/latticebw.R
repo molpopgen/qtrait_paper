09ccc1a419a27f95d33dccfb17c19600e6b1579c
molpopgen <krthornt@uci.edu>
2017-10-02 14:04:02 -0700

    updates to plotting scripts

---
+++ sqlite/R/plots/latticebw.R
@@ -0,0 +1,6 @@
+library(lattice)
+
+bwtheme <- standard.theme("pdf", color=FALSE)
+bwtheme$plot.line$lwd <- 3
+bwtheme$superpose.line$lwd <- 3
+bwtheme$fontsize$text <- 14
--- sqlite/R/plots/traits.R
+++ sqlite/R/plots/traits.R
@@ -1,6 +1,7 @@
 library(data.table)
 library(lattice)
 library(ggplot2)
+source("latticebw.R")
 
 muvals = c("0.00025", "0.001", "0.005")
 optvals = c("0.1","0.5","1")
@@ -24,18 +25,43 @@ for (m in muvals)
 }
 
 #Lattice graphics, FTW
-bwtheme <- standard.theme("pdf", color=FALSE)
-
-pdf("slocusVG.pdf")
-print(paste("z","=",as.numeric(optvals)))
 data$scaled_time = (data$generation-50000)/5000.0
-xyplot(VG~scaled_time|as.factor(opt),group=mu,data=data,type='l',layout=c(1,3),
-       xlim=c(-0.1,0.25),
+XLIM=c(-0.02,0.1)
+STRIP=strip.custom(var.name=expression(z[0]),strip.levels=c(T,T),sep=" = ",bg=0,fg=0,style=1)
+XLAB="Time since optimum shift (units of N generation)"
+LWD=c(3,3,3)
+LTY=c("solid","dashed","dotdash")
+KEYTEXT = c(expression(paste(mu," = ",2.5e-4)),
+            expression(paste(mu," = ",1e-3)),        
+            expression(paste(mu," = ",5e-3)))
+KEY = list(
+           space = "top", points = FALSE, lines = TRUE,
+           text=KEYTEXT,
+            columns = 3)
+VGplot=xyplot(VG~scaled_time|opt,group=mu,data=data,type='l',layout=c(1,3),
+       xlim=XLIM,
        scales=list(x=list(tick.number=10)),
-       xlab="Time since optimum shift (units of N generation)",
-       ylab=expression(V[G]),
+       xlab=XLAB,
+       ylab="Genetic variance",
        par.settings=bwtheme,
-       strip=strip.custom(var.name=expression(z[0]),strip.levels=c(T,T),sep=" = ",bg=0),
-       lwd=c(3,3,3)
+       strip=STRIP,
+       auto.key = KEY
+       )
+pdf("slocusVG.pdf")
+VGplot
+dev.off()
+tiff("slocusVG.tiff")
+VGplot
+dev.off()
+
+pdf("slocusZbar.pdf")
+xyplot(tbar~scaled_time|opt,group=mu,data=data,type='l',layout=c(1,3),
+       xlim=XLIM,
+       scales=list(x=list(tick.number=10)),
+       xlab=XLAB,
+       ylab=expression(bar(z)),
+       par.settings=bwtheme,
+       strip=STRIP,
+       auto.key = KEY,
        )
 dev.off()
