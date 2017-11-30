2deb2f220f4d18ed0d475d32d95d061ebba3b822
molpopgen <krthornt@uci.edu>
2017-11-30 09:11:11 -0800

    fixes to SweeD submission script

--- hpc/run_mlocus11_sweed.sh
+++ hpc/run_mlocus11_sweed.sh
@@ -5,7 +5,7 @@
 
 module load krthornt/anaconda
 
-cd $SGE_O_WORKDIR
+cd $SGE_O_WORKDIR/mlocus_pickle
 
 INFILE=$1
 OUTFILE=$2
@@ -13,4 +13,4 @@ MU=`echo $INFILE | sed 's/^.*u//' | sed 's/\.opt.*//'`
 OPT=`echo $INFILE | sed 's/^.*opt//' | sed 's/\.tar//'`
 echo "OPT=$OPT"
 P=`echo "$CORES/2" | bc`
-python python/mlocus11_sweed.py --tarfile $INFILE --outfile $OUTFILE --mu $MU --opt $OPT --nprocs $P
+python ../python/mlocus11_sweed.py --tarfile $INFILE --outfile $OUTFILE --mu $MU --opt $OPT --nprocs $P
--- hpc/sub_mlocus11_sweed.sh
+++ hpc/sub_mlocus11_sweed.sh
@@ -3,18 +3,18 @@
 NJOBS=1
 for i in mlocus_pickle/pop*.tar
 do
-    n=`basename $i .db`
+    n=`basename $i .tar`
     echo $i $n
     NAME="SWEEDJOB_"$NJOBS
     if [ $NJOBS -lt 4 ]
     then
         echo $NAME
-        qsub -N $NAME hpc/run_mlocus11_sweed.sh $i mlocus_pickle/$n"_sweed.db"
+        qsub -N $NAME hpc/run_mlocus11_sweed.sh $n.tar $n"_sweed.db"
     else
         X=$(($NJOBS-3))
         LASTJOB="SWEEDJOB_"$X
         echo $NAME $LASTJOB
-        qsub -N $NAME -hold_jid $LASTJOB hpc/run_mlocus11_sweed.sh $i mlocus_pickle/$n"_sweed.db"
+        qsub -N $NAME -hold_jid $LASTJOB hpc/run_mlocus11_sweed.sh $n.tar $n"_sweed.db"
     fi
     NJOBS=$(($NJOBS+1))
 done
