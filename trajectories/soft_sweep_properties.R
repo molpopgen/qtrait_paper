aab03447b0e8eef435a00a2179f31cc89bdf598b
molpopgen <krthornt@uci.edu>
2018-01-15 15:32:22 -0800

    fix legend and plot freq at shift vs age. this plot looks wacky, though...

--- trajectories/soft_sweep_properties.R
+++ trajectories/soft_sweep_properties.R
@@ -45,7 +45,7 @@ for(i in files)
 KEY=list(space="top",columns=3,title="",
          cex.title=1,points=FALSE,lines=TRUE,just=0.5)
 STRIP=strip.custom(strip.names = TRUE, 
-                   var.name = c(expression(z[o]),expression(mu)),bg=c("white"))
+                   var.name = c(expression(mu),expression(z[o])),bg=c("white"))
 #COLORS=c("black")
 
 p = xyplot(esize~freq_at_shift|as.factor(mu)*as.factor(opt),data=data,
@@ -65,5 +65,30 @@ p = xyplot(esize~freq_at_shift|as.factor(mu)*as.factor(opt),data=data,
 
 
 trellis.device(device="pdf",file="SoftSweepFreqEsize.pdf",height=10,width=10)
+print(p)
+dev.off()
+
+y = data %>% group_by(mu,opt) %>%
+    summarise(mo=min(origin))
+
+print(y)
+
+p = xyplot((50000-origin)/5e3~freq_at_shift|as.factor(mu)*as.factor(opt),data=data,
+           mu = data$mu,
+          par.settings=simpleTheme(pch=16,cex=0.5,alpha.points=0.5),
+          auto.key=KEY, xlab="Mutation frequency at optimum shift",
+          ylab=expression("Age of mutation (units of N generations"),
+          scales=list(cex=1,alternating=F,x=list(rot=45)),
+          strip=STRIP,
+           panel=function(x, y,mu,...,subscripts){
+            panel.xyplot(x,y)
+            panel_mu = unique(mu[subscripts])
+           ghat = 2*sqrt(2)*sqrt(panel_mu)
+            panel.abline(h=ghat)
+    })
+
+
+
+trellis.device(device="pdf",file="SoftSweepFreqAge.pdf",height=10,width=10)
 print(p)
 dev.off()
