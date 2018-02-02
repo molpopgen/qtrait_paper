35c1fc2e883f4ae2f6fa189286f61fd959b49c15
molpopgen <krthornt@uci.edu>
2018-02-02 12:28:14 -0800

    add horizontal line for mutations of large effect with negative effect sizes

--- trajectories/soft_sweep_properties.R
+++ trajectories/soft_sweep_properties.R
@@ -60,6 +60,7 @@ p = xyplot(esize~freq_at_shift|as.factor(mu)*as.factor(opt),data=data,
             panel_mu = unique(mu[subscripts])
            ghat = 2*sqrt(2)*sqrt(panel_mu)
             panel.abline(h=ghat,lwd=2)
+            panel.abline(h=-ghat,lwd=2)
             gamma_overshoots = unique(optima[subscripts])/2
             panel.abline(h=gamma_overshoots,lty="dotdash",lwd=2)
     })
@@ -87,6 +88,7 @@ p = xyplot((50000-origin)/5e3~freq_at_shift|as.factor(mu)*as.factor(opt),data=da
             panel_mu = unique(mu[subscripts])
            ghat = 2*sqrt(2)*sqrt(panel_mu)
             panel.abline(h=ghat,lwd=2)
+            panel.abline(h=-ghat,lwd=2)
     })
 
 
