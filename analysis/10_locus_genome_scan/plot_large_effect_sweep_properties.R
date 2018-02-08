9c8ab7c965c2c7768d60a16cbea8692cd3ba6424
molpopgen <krthornt@uci.edu>
2018-02-08 15:50:22 -0800

    new figures summarizing effect sizes and origin time of large effect substitutions

--- analysis/10_locus_genome_scan/plot_large_effect_sweep_properties.R
+++ analysis/10_locus_genome_scan/plot_large_effect_sweep_properties.R
@@ -12,14 +12,40 @@ save_image <- function(stat,img)
     print(img)
     dev.off()
 }
+KEY=list(space="top",columns=3,title="Distance from window with causal mutations.",
+         cex.title=1,points=FALSE,lines=TRUE,just=0.5)
+STRIP=strip.custom(strip.names = TRUE,sep=" = ", 
+                   var.name = c(expression(mu),expression(z[o])),bg=c("white"))
 #plot origin times of these events
 fixations_unprocessed=fixations_raw%>%filter(sweep_type!="none",
            abs(s) >= 2*sqrt(2)*sqrt(mu),(g-5e4)/g < 0.01)
-dp = densityplot(~s|as.factor(mu)*as.factor(opt),data=fixations_unprocessed,groups=sweep_type,
-                 auto.key=TRUE)
-save_image("esizes_large_effect_sweeps",dp)
-p = xyplot(s~abs((g-5e4)/5e4)|as.factor(mu)*as.factor(opt),data=fixations_unprocessed,
-           groups=sweep_type,
-           auto.key=TRUE)
-save_image("esizes_vs_distance_from_shift_large_effect_sweeps",p)
+fixations_unprocessed = fixations_unprocessed %>%
+    group_by(sweep_type) %>%
+    mutate(ST = ifelse(sweep_type == 'hard',"Hard\nsweep","Soft\nsweep"))
+print(range(fixations_unprocessed$s))
+dp = bwplot(s~ST|as.factor(mu)*as.factor(opt),data=fixations_unprocessed,
+                 auto.key=KEY,
+                 strip=STRIP,
+                  scales=list(cex=1,alternating=F),
+                 panel = function(..., box.ratio) {
+         panel.stripplot(..., jitter.data = T,pch=19,cex=0.5,alpha=0.2,col=viridis(1))
+         panel.violin(..., col = "transparent", border = "grey60", varwidth = FALSE, 
+             box.ratio = box.ratio)
+    },
+    ylab=expression(paste("Effect size (",gamma,")",sep="")))
+save_image("LargeEffectSizeFixationsEsizeViolin",dp)
+# p = xyplot(s~abs((g-5e4)/5e4)|as.factor(mu)*as.factor(opt),data=fixations_unprocessed,
+#            groups=sweep_type,
+#            auto.key=TRUE)
+p = bwplot(abs((g-5e4)/5e4)~ST|as.factor(mu)*as.factor(opt),data=fixations_unprocessed,
+           auto.key=KEY,
+           scales=list(y=list(relation="free")),
+           strip=STRIP,
+                 panel = function(..., box.ratio) {
+         panel.stripplot(..., jitter.data = T,pch=19,cex=0.5,alpha=0.2,col=viridis(1))
+         panel.violin(..., col = "transparent", border = "grey60", varwidth = FALSE, 
+             box.ratio = box.ratio)
+    },
+    ylab=expression("|Time| since optimum shift (units of N generations)"))
+save_image("LargeEffectSizeFixationsTimeViolin",p)
 
