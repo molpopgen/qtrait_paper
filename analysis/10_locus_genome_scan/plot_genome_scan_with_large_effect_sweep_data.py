db389a397fe1894c67ac9dbd9ecd4ef5c7479147
molpopgen <krthornt@uci.edu>
2018-02-12 07:01:58 -0800

    fix row order and x-axis category label

--- analysis/10_locus_genome_scan/plot_genome_scan_with_large_effect_sweep_data.py
+++ analysis/10_locus_genome_scan/plot_genome_scan_with_large_effect_sweep_data.py
@@ -40,8 +40,10 @@ p = {i:colormap(normalize(scipy.stats.norm.cdf(i,scale=0.25))) for i in df.s}
 # gs = gridspec.GridSpec(1,1)
 # gs.update(left=0.1, right=0.8, wspace=0.05)
 # axes = [plt.subplot(i) for i in gs]
-fp = sns.factorplot(x='detailed_type',y='scaled_time',
+fp = sns.factorplot(x='sweep_type',y='scaled_time',
         col='mu',row='opt',data=df,hue='s',palette=p,
+        row_order=[1.0,0.5,0.1],
+        #row_order=[i for i in df.opt.unique()].reverse(),
         kind='swarm',legend=False)
 plt.subplots_adjust(left=0.1, right=0.8, wspace=0.05)
 for ax in fp.axes.flatten():
