636838765ee7d682ecc1df1a4d3bc49c5650a670
molpopgen <krthornt@uci.edu>
2018-02-15 13:53:34 -0800

    change factor order for raw sweed power calc

--- analysis/10_locus_genome_scan/plot_raw_SweeD.R
+++ analysis/10_locus_genome_scan/plot_raw_SweeD.R
@@ -10,25 +10,25 @@ x = x %>% group_by(generation,mu,opt) %>%
     mutate(scaled_time = (generation - 50000)/50000)
 
 STRIP=strip.custom(strip.names = TRUE, 
-                   var.name = c(expression(z[o]),expression(mu)),bg=c("white"))
-LRplot = xyplot(meanLR ~ scaled_time| as.factor(opt)*as.factor(mu),
+                   var.name = c(expression(mu),expression(opt)),bg=c("white"))
+LRplot = xyplot(meanLR ~ scaled_time| as.factor(mu)*as.factor(opt),
                 type='l',data=x,
                   xlab="Time since optimum shift (units of N generations)",
                   ylab="Mean composite likelihood ratio statistic",
                   scales=list(cex=1),
                   strip=STRIP)
 
-trellis.device(device="pdf",file="Sweed_LR_raw.pdf",height=10,width=10, color=FALSE)
+trellis.device(device="pdf",file="SweeDLRRaw.pdf",height=10,width=10, color=FALSE)
 print(LRplot)
 dev.off()
 
-alpha_plot = xyplot(mean_alpha ~ scaled_time| as.factor(opt)*as.factor(mu),
+alpha_plot = xyplot(mean_alpha ~ scaled_time| as.factor(mu)*as.factor(opt),
                 type='l',data=x,
                   xlab="Time since optimum shift (units of N generations)",
                   ylab="Mean estimate of 2Ns",
                   scales=list(cex=1),
                   strip=STRIP)
 
-trellis.device(device="pdf",file="Sweed_alpha_raw.pdf",height=10,width=10, color=FALSE)
+trellis.device(device="pdf",file="SweedAlphaRaw.pdf",height=10,width=10, color=FALSE)
 print(alpha_plot)
 dev.off()
