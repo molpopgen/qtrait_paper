a185b0bdcff2739630c60f71c45a25346212192c
molpopgen <krthornt@uci.edu>
2018-02-22 15:35:03 -0800

    added scripts to submit sims to further explore parameters

---
+++ hpc/mlocus11_xtra.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#$ -q krt,krti,bio
+#$ -pe openmp 64-72
+#$ -ckpt blcr
+
+module load krthornt/anaconda/3
+
+cd $SGE_O_WORKDIR
+
+mu=$1
+opt=$2
+seed=$3
+plarge=$4
+vsopt=$5
+db1=$6
+db2=$7
+
+python python/multi_region11_explore.py --ncores $CORES --nreps 256 --mu $mu --opt $opt --seed $seed --plarge $plarge --vsopt \
+    $vsopt --statsdb $db1 --scandb $db2
+
---
+++ hpc/sub_mlocus11_xtra.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+for mu in 0.00025 0.005
+do
+	for opt in 1.0
+    do
+        for plarge in 0.01 0.10
+        do
+            for vsopt in 1 0.5 0.1
+            do
+                db1=mu"$mu$".opt"$opt".plarge"$plarge".vsopt"$vsopt".stats.sqlite3
+                db2=mu"$mu$".opt"$opt".plarge"$plarge".vsopt"$vsopt".scan.sqlite3
+                qsub -q krti,krt,bio hpc/mlocus11_xtra.sh $mu $opt $RANDOM $plarge $vsopt mlocus11_xtra_sims/$db1 mlocus11_xtra_sims/$db2
+            done
+        done
+    done
+done
