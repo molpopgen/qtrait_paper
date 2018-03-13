9f904797e047a37cdcb266070b8b6e050f874ec0
molpopgen <krthornt@uci.edu>
2018-03-13 08:37:44 -0700

    make freq vs esize prior to shift figs based on single rep.  add compbined fig

--- trajectories/plot_esize_freq_at_msb.py
+++ trajectories/plot_esize_freq_at_msb.py
@@ -13,14 +13,16 @@ files = glob.glob('*_traj_merged.db')
 dfs = []
 for fi in files:
     s = fi.split("_")
-    mu = float(s[5])
-    print(fi,mu)
-    c = sqlite3.connect(fi)
-    df = pd.read_sql('select repid,freq,origin,esize from freqs where generation == 50000',c) # group by repid,pos,origin',c)
-    df.drop_duplicates(inplace=True)
-    df['mu']=[mu]*len(df.index)
-    dfs.append(df)
-    c.close()
+    opt = s[3]
+    if opt == '1':
+        mu = float(s[5])
+        print(fi,mu)
+        c = sqlite3.connect(fi)
+        df = pd.read_sql('select repid,freq,origin,esize from freqs where repid < 1 and generation == 50000',c) # group by repid,pos,origin',c)
+        df.drop_duplicates(inplace=True)
+        df['mu']=[mu]*len(df.index)
+        dfs.append(df)
+        c.close()
 
 df = pd.concat(dfs)
 
@@ -28,11 +30,12 @@ gr = df.groupby(['mu'])
 
 I=0
 for n,g in gr:
-    print(n)
+    print(n,len(g.index))
     fig = plt.figure(figsize=(10, 10))
     p = sns.jointplot('esize','freq', data=g, kind='hex',
                       stat_func=None, xlim=(-1,1),ylim=(0,0.2),
-                      space=0)
+                      space=0)#,joint_kws=dict(bins=len(g.index)))
+    p.ax_joint.text(-0.9,0.185,r'$\mu = $'+'{0:0.5f}'.format(n))
     plt.subplots_adjust(right=0.9)
     cbaxes = inset_axes(p.ax_joint, width="45%", height="3%", loc=1)
     cbar = plt.colorbar(cax=cbaxes, orientation='horizontal')
