be942c2adf853f5bf34b3e2137a970f897cc8e51
molpopgen <krthornt@uci.edu>
2018-03-22 18:00:26 -0700

    give unconditional power plots the new, improved look

--- analysis/10_locus_genome_scan/plot_unconditional_power.R
+++ analysis/10_locus_genome_scan/plot_unconditional_power.R
@@ -14,8 +14,8 @@ save_image <- function(stat,img)
     dev.off()
 }
 
-KEY=list(space="top",columns=3,title="Distance from window with causal mutations.",
-         cex.title=1,points=FALSE,lines=TRUE,just=0.5)
+# KEY=list(space="top",columns=3,title="Distance from window with causal mutations.",
+#          cex.title=1,points=FALSE,lines=TRUE,just=0.5)
 STRIP=strip.custom(strip.names = TRUE,sep=" = ", 
                    var.name = c(expression(mu),expression(z[o])),bg=c("white"))
 
@@ -33,12 +33,37 @@ for(SI in unique(p$stat))
                # of observations.
                adjustment = ifelse(dist == 0,1,2))
 
+    print(sort(unique(s$dist)))
     COLORS=viridis(length(unique(as.factor(s$dist))))
+    ICOLORS=viridis(length(unique(as.factor(s$dist))))
+    COLORS=array()
+    COLORS[1] = rgb(as.integer(col2rgb(ICOLORS[1])[1,])/255,
+                as.integer(col2rgb(ICOLORS[1])[2,])/255,
+                as.integer(col2rgb(ICOLORS[1])[3,])/255,
+                alpha=1/length(ICOLORS))
+    for(i in 2:length(ICOLORS))
+    {
+        ncolor = rgb(as.integer(col2rgb(ICOLORS[i])[1,])/255,
+                    as.integer(col2rgb(ICOLORS[i])[2,])/255,
+                    as.integer(col2rgb(ICOLORS[i])[3,])/255,
+                    alpha=i/length(ICOLORS))
+        COLORS[i]=ncolor
+    }
+    nlines = length(unique(s$dist))
+    KEY=list(space="top",columns=3,
+             title="Distance from window with causal mutations.",
+             cex.title=1,#points=FALSE,
+             lines=list(lwd=rep(3,length(COLORS)),col=rev(COLORS)),
+             just=0.5,
+             text=list(as.character(sort(unique(s$dist)))))
+
     PLOT = xyplot(total/(adjustment*256*11)~scaled_time | as.factor(mu)*as.factor(opt),
-                  group=dist,data=s,type='l',
+                  lwd=3,data=s,type='l',
+                  group=factor(dist,levels=rev(sort(unique(s$dist)))),
+                  #group=dist,data=s,type='l',
                   xlim=XLIM,
-                  par.settings=simpleTheme(col=COLORS,lwd=3),
-                  auto.key=KEY,
+                  par.settings=simpleTheme(col=COLORS),
+                  key=KEY,
                   scales=list(cex=1,alternating=F),
                   strip=STRIP,
                   xlab="Time since optimum shift (units of N generations)",
@@ -49,3 +74,4 @@ for(SI in unique(p$stat))
     FN=paste("Power",STAT,sep="")
     save_image(FN,PLOT)
 }
+
