02834f7426407210e7540d412474c56e52e4fadc
molpopgen <krthornt@uci.edu>
2018-04-17 14:02:14 -0700

    add scripts for MIRA plots

---
+++ analysis/10_locus_genome_scan/genetic_value_per_locus_MIRA.R
@@ -0,0 +1,73 @@
+library(stringr)
+library(readr)
+library(tidyr)
+library(dplyr)
+library(lattice)
+library(viridis)
+
+process_gvalues <- function (filename)
+{
+    fs <- str_split(filename,regex('\\.'))[[1]]
+    fs[2]<-str_replace(fs[2],'mu','')
+    fs[4]<-str_replace(fs[4],'opt','')
+    dbmu <- as.numeric(paste(fs[2],fs[3],sep='.'))
+    dbopt <- as.numeric(paste(fs[4],fs[5],sep='.'))
+
+    dbname = paste("../../mlocus_pickle/",filename,sep="")
+    if(!file.exists(dbname))
+    {
+        stop("database not found")
+    } 
+    db = DBI::dbConnect(RSQLite::SQLite(),dbname)
+    dbt = tbl(db,'data')
+
+    g = collect(dbt) %>% 
+        group_by(repid,generation) %>%
+        mutate(rank = dense_rank(desc(abs(g)))) %>%
+        group_by(generation,rank) %>%
+        summarise(mg = mean(g),mag=mean(abs(g))) %>%
+        mutate(mu=dbmu,opt=dbopt)
+    DBI::dbDisconnect(db)
+    g
+}
+
+files <- dir(path="../../mlocus_pickle/",pattern="*.genetic_values_per_locus.db")
+
+d = NA
+for (i in files)
+{
+    x = process_gvalues(i)
+    if(is.na(d))
+    {
+        d=x
+    }
+    else
+    {
+        d=rbind(d,x)
+    }
+}
+print(warnings())
+d = d %>% filter(opt==1) %>%
+    mutate(scaled_time = (generation-5e4)/5e3)
+
+KEY=list(space="top",columns=3,title="",
+         cex.title=1,points=FALSE,lines=TRUE,just=0.5)
+STRIP=strip.custom(strip.names = TRUE, sep=" = ",
+                   var.name = c(expression(mu),expression(z[o])),bg=c("white"))
+COLORS=rev(viridis(length(unique(as.factor(d$rank)))))
+p = xyplot((mg/opt)~scaled_time|as.factor(mu)*as.factor(opt),
+           type='l',
+           group=rank,
+           strip=STRIP,
+           #auto.key=KEY,
+           par.settings=simpleTheme(col=COLORS,lwd=3),
+           scales=list(cex=1,alternating=F),
+           xlab="Time since optimum shift (units of N generations)",
+           ylab=expression(paste("Contribution of locus to adaptation, ",over(bar(g), z[o]))),
+           xlim=c(-0.1,0.25),
+           ylim=c(0.0,1.1),
+           data=d)
+trellis.device(device="pdf",file="MeanGeneticValuePerLocusMIRA.pdf",height=4,width=10)
+trellis.par.set("fontsize",list(text=12))
+print(p)
+dev.off()
---
+++ analysis/10_locus_genome_scan/plot_genome_scan_with_large_effect_sweep_dataMIRA.R
@@ -0,0 +1,79 @@
+library(dbplyr)
+library(dplyr)
+library(tidyr)
+library(lattice)
+library(viridis)
+
+dbf = src_sqlite("genome_scan_with_large_effect_sweep_counts.sqlite3")
+dbt = tbl(dbf,'data')
+
+hard_only = dbt %>%
+    select(-c(repid, locus)) %>%
+    filter(nhard>0,nsoft==0,opt>0.1) %>%
+    mutate(dist = abs(window-5)) %>%
+    group_by(generation,window,dist,opt,mu) %>%
+    summarise_all(funs(mean)) %>% 
+    mutate(scaled_time = (generation-50000)/5000) 
+soft_only = dbt %>%
+    select(-c(repid, locus)) %>%
+    filter(nhard==0,nsoft>0,opt>0.1) %>%
+    mutate(dist = abs(window-5)) %>%
+    group_by(generation,window,dist,opt,mu) %>%
+    summarise_all(funs(mean)) %>% 
+    mutate(scaled_time = (generation-50000)/5000) 
+
+hard_only=collect(hard_only)
+soft_only=collect(soft_only)
+
+X=rbind(hard_only,soft_only)
+X=X%>%
+    mutate(type=ifelse(nhard>0,'New mutation','Standing var.'))
+
+# COLORS=rev(viridis(length(unique(as.factor(hard_only$dist)))))
+# KEY=list(space="top",columns=3,title="Distance from window with causal mutations.",
+#          cex.title=1,points=FALSE,lines=TRUE,just=0.5)
+ICOLORS=viridis(length(unique(as.factor(X$dist))))
+COLORS=array()
+COLORS[1] = rgb(as.integer(col2rgb(ICOLORS[1])[1,])/255,
+            as.integer(col2rgb(ICOLORS[1])[2,])/255,
+            as.integer(col2rgb(ICOLORS[1])[3,])/255,
+            alpha=1/length(ICOLORS))
+for(i in 2:length(ICOLORS))
+{
+    ncolor = rgb(as.integer(col2rgb(ICOLORS[i])[1,])/255,
+                as.integer(col2rgb(ICOLORS[i])[2,])/255,
+                as.integer(col2rgb(ICOLORS[i])[3,])/255,
+                alpha=i/length(ICOLORS))
+    COLORS[i]=ncolor
+}
+KEY=list(space="top",columns=3,
+         title="Distance from window with causal mutations.",
+         cex.title=1,#points=FALSE,
+         lines=list(lwd=rep(3,length(COLORS)),col=rev(COLORS)),
+         just=0.5,
+         text=list(as.character(sort(unique(X$dist)))))
+STRIP=strip.custom(strip.names = TRUE,sep=" = ", 
+                   var.name = c(expression(mu),expression(z[o])),bg=c("white"))
+
+save_image <- function(stat,img)
+{
+    trellis.device(device="pdf",file=paste(stat,".pdf",sep=""),height=5,width=10)
+    trellis.par.set("fontsize",list(text=12))
+    print(img)
+    dev.off()
+}
+
+X=subset(X,opt==1.0)
+
+hprimePlot = xyplot(hprime ~ scaled_time| as.factor(mu)*as.factor(opt):as.factor(type),
+                  group=factor(dist,levels=rev(sort(unique(X$dist)))),
+                  type='l',data=X,
+                  par.settings=simpleTheme(col=COLORS),
+                  key=KEY, lwd=3,
+                  xlab="Time since optimum shift (units of N generations)",
+                  ylab="Mean H'",
+                  xlim=c(-1,5),
+                  scales=list(cex=1,alternating=F),
+                  strip=STRIP)
+save_image('MeanHprimeTenLociLargeEffectOnlyMIRA',hprimePlot)
+
