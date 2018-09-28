75a0f27201aadb68ef14e4ceed68c03dbb065952
molpopgen <krthornt@uci.edu>
2018-09-28 10:26:54 -0700

    Add new SGE scripts

---
+++ mlocus_vary_plarge/run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#$ -q krti,krt,bsg2,bio
+#$ -pe openmp 64-128
+
+cd $SGE_O_WORKDIR
+module load krthornt/anaconda
+source activate fwdpy11_0_2_0
+PLARGE=$1
+MU=$2
+QO=$3
+GO=$4
+FO=$5
+
+python3 ../python/mlocus11_vary_plarge.py --N 5000 --mu $MU \
+    --plarge $PLARGE \
+    --nsam 50 \
+    --nreps 128 \
+    --ncores $CORES \
+    --opt 1.0 \
+    --seed $RANDOM \
+    --qoutfile $QO \
+    --goutfile $GO \
+    --foutfile $FO
---
+++ mlocus_vary_plarge/sub.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for mu in 0.005 0.001 0.00025
+do
+    for plarge in 0.05 0.1 0.25 0.5 0.75
+    do
+        SUFFIX=mu$mu".plarge"$plarge".opt1.sqlite3"
+        echo $SUFFIX
+        qsub run.sh $plarge $mu qtrait.$SUFFIX gscan.$SUFFIX fixations.$SUFFIX
+    done
+done
