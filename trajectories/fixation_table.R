60e4d18781ee346739d7f416c346d68b6d607d14
molpopgen <krthornt@uci.edu>
2018-12-10 10:57:13 -0800

    updates to tables

--- trajectories/fixation_table.R
+++ trajectories/fixation_table.R
@@ -11,7 +11,7 @@ soft=collect(dbt) %>%
     filter(origin+sojourn_time-1>50000)%>%
     mutate(n=n())%>%
     select(-pos,-origin) %>%
-    group_by(opt,mu,large) %>%
+    group_by(mu,opt,large) %>%
     summarise(mesize_soft=mean(abs(esize)),msojourn_soft=mean(sojourn_time)/5e3,mean_soft=mean(n))
 
 hard=collect(dbt) %>%
@@ -20,7 +20,7 @@ hard=collect(dbt) %>%
     filter(origin>50000 & origin < 5e4 + 1e2)%>%
     mutate(n=n())%>%
     select(-pos,-origin) %>%
-    group_by(opt,mu,large) %>%
+    group_by(mu,opt,large) %>%
     summarise(mesize_hard=mean(abs(esize)),msojourn_hard=mean(sojourn_time)/5e3,mean_hard=mean(n))
 
 old=collect(dbt) %>%
@@ -32,7 +32,8 @@ old=collect(dbt) %>%
     group_by(opt,mu,large) %>%
     summarise(mesize_old=mean(abs(esize)),msojourn_old=mean(sojourn_time)/5e3,mean_old=mean(n))
 
-final = left_join(soft,hard,by=c("opt","mu","large"))
+final = left_join(soft,hard,by=c("opt","mu","large")) %>%
+    arrange(mu,opt,large)
 # final = left_join(final,old,by=c("opt","mu","large"))
 final$large = as.integer(final$large)
 # final<-soft
