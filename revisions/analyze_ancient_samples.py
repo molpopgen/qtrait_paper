2288db619626608e598c9f3aca66c5bdccf3e9c2
molpopgen <krthornt@uci.edu>
2019-03-15 13:55:21 -0700

    1. Seed np.random 2. Simplify LD code 3. Move genome scan code to sperate fxn

--- revisions/analyze_ancient_samples.py
+++ revisions/analyze_ancient_samples.py
@@ -20,6 +20,10 @@ GenomeScanRecord = namedtuple(
 
 QtraitRecord = namedtuple('QtraitRecord', ['generation', 'zbar', 'wbar', 'vg'])
 
+# NOTE: this implies that we'll be taking means of means later on!
+LDRecord = namedtuple('LDRecord', ['generation', 'intralocus_zns', 'interlocus_zns',
+                                   'intralocus_D', 'interlocus_D'])
+
 
 def make_parser():
     parser = argparse.ArgumentParser()
@@ -31,6 +35,8 @@ def make_parser():
                         help="sqlite3 file name for genome scan output")
     parser.add_argument('qtrait_dbfile', type=str,
                         help="sqlite3 file name for quant get output")
+    parser.add_argument('ld_dbfile', type=str,
+                        help="sqlite3 file name for quant get output")
     parser.add_argument('seed', type=int, default=None, help="RNG seed")
 
     parser.add_argument('--theta', type=float,
@@ -40,25 +46,34 @@ def make_parser():
     return parser
 
 
-def pairwise_LD(selected):
+def pairwise_LD(t, pop, tables, samples, idmap):
     """
     Returns D and r^2 for all pairs of sites,
     along with site metadata
     """
+    gm = fwdpy11.ts.data_matrix_from_tables(
+        tables, pop.mutations, idmap[samples], False, True)
+    selected = gm.selected
     nsites = selected.shape[0]
     nsam = selected.shape[1]
-    rsq_indexes = np.triu_indices(nsites, 1)
 
     pos = np.array(selected.positions)
     data = np.array(selected, copy=False)
+    daf = np.sum(data, axis=1) / nsam
+    daf_filter = np.where((daf >= 1e-2) & (daf < 1.0))[0]
+
+    pos = pos[daf_filter]
+    data = data[daf_filter, :]
+    daf = daf[daf_filter]
+
+    nsites = len(daf_filter)
+    rsq_indexes = np.triu_indices(nsites, 1)
+
     rsq = np.ones(len(data[rsq_indexes]))
     D = np.zeros(len(data[rsq_indexes]))
     pos1 = np.zeros(len(rsq))
     pos2 = np.zeros(len(rsq))
-    daf1 = np.zeros(len(rsq))
-    daf2 = np.zeros(len(rsq))
     idx = 0
-    daf = np.sum(data, axis=1) / nsam
     for i in range(nsites - 1):
         for j in range(i + 1, nsites):
             p0 = daf[i]
@@ -67,8 +82,6 @@ def pairwise_LD(selected):
             p11 = len(np.where(temp == 2)[0]) / nsam
             pos1[idx] = pos[i]
             pos2[idx] = pos[j]
-            daf1[idx] = daf[i]
-            daf2[idx] = daf[j]
             D[idx] = p11 - p0 * p1
             rsq[idx] = np.power(D[idx], 2.0) / \
                 (p0 * (1.0 - p0) * p1 * (1.0 - p1))
@@ -86,16 +99,70 @@ def pairwise_LD(selected):
             locus2[w2] = locus
 
     intralocus = np.zeros(len(rsq), dtype=np.int32)
-    intralocus[np.where(locus1==locus2)[0]] = 1
-    df = pd.DataFrame({'pos1': pos1,
-                       'pos2': pos2,
-                       'daf1': daf1,
-                       'daf2': daf2,
-                       'rsq': rsq,
-                       'D': D,
-                       'intralocus':intralocus})
+    intralocus[np.where(locus1 == locus2)[0]] = 1
+    intralocus_pairs = np.where(intralocus == 1)[0]
+    interlocus_pairs = np.where(intralocus == 0)[0]
+    return LDRecord(t, rsq[intralocus_pairs].mean(), rsq[interlocus_pairs].mean(),
+                    D[intralocus_pairs].mean(), rsq[interlocus_pairs].mean())
 
-    return df
+
+def genome_scan_stats(t, mutations, tables, idmap, amd, sample_indexes_at_time):
+    # Get a random subset of the ancient samples
+    # for our genome scan
+    random_sample = np.random.choice(
+        sample_indexes_at_time, args.nsam, replace=False)
+    # Get the nodes associated w/those random samples
+    samples = amd['nodes'][random_sample].flatten()
+    remapped = idmap[samples]
+
+    # # Simplify the tables w.r.to the samples.
+    # # Simplification results in a much smaller
+    # # set of trees to iterate over.
+    # tables, idmap = fwdpy11.ts.simplify(pop, samples)
+
+    # # These are the new indexes of our sample, after
+    # # simplification
+    # remapped = idmap[samples]
+
+    # Now, we build a numpy array of the genotypes
+    # containing both selected an neutral variants.
+    # Because we use np.concatenate, to join
+    # the neutral and selected sites, we have to be careful
+    # to create windows where the sites are in the correct
+    # order, which is increasing by position.  To
+    # accomplish this, we rely on np.argsort
+    gm = fwdpy11.ts.data_matrix_from_tables(
+        tables, mutations, remapped, True, True)
+
+    pos = np.array(gm.neutral.positions + gm.selected.positions)
+    sorted_pos_indexes = np.argsort(pos)
+    all_sites = np.concatenate(
+        (np.array(gm.neutral, copy=False), np.array(gm.selected, copy=False)))
+    # We no longer need this object, and it is big.
+    # reorder the arrays
+    pos = pos[sorted_pos_indexes]
+    all_sites = all_sites[sorted_pos_indexes, :]
+
+    # Now, we get a big libsequence object...
+    vm = vmatrix.VariantMatrix(all_sites, pos)
+    # ... and count the alleles
+    ac = vm.count_alleles()
+    # Now, we create our windows and iterate over them
+    # via numpy "fancy" indexing methods:
+    rv = []
+    for locus, start_stop in enumerate(LOCUS_BOUNDARIES):
+        for window, start in enumerate(range(*start_stop)):
+            sites_in_window = np.where((pos >= start) & (
+                pos[sorted_pos_indexes] < start + 1.))[0]
+            acw = ac[sites_in_window]
+            pi = sstats.thetapi(acw)
+            tajd = sstats.tajd(acw)
+            hprime = sstats.hprime(acw, 0)
+            rv.append(GenomeScanRecord(
+                t, locus, window, pi, tajd, hprime))
+
+    return rv
+
 
 def process_replicate(argtuple):
     filename, repid, seed = argtuple
@@ -104,6 +171,7 @@ def process_replicate(argtuple):
         pop = fwdpy11.SlocusPop.load_from_pickle_file(f)
 
     rng = fwdpy11.GSLrng(seed)
+    np.random.seed(seed)
 
     # Add neutral mutations
     nm = fwdpy11.ts.infinite_sites(
@@ -118,6 +186,7 @@ def process_replicate(argtuple):
 
     genome_scan = []
     qtraits = []
+    ld = []
     for t in np.unique(amt):
         # Get the indexes of the metadata corresponding
         # to this time point
@@ -129,74 +198,15 @@ def process_replicate(argtuple):
         wbar = amd['w'][sample_indexes_at_time].mean()
         qtraits.append(QtraitRecord(t, mean_trait_value, wbar, vg))
 
-        # TODO: we need to do something here
-        # about LD amongst selected variants
         samples = amd['nodes'][sample_indexes_at_time].flatten()
         tables, idmap = fwdpy11.ts.simplify(pop, samples)
-        gm = fwdpy11.ts.data_matrix_from_tables(
-            tables, pop.mutations, idmap[samples], False, True)
 
-        ld = pairwise_LD(gm.selected)
-        print(ld.head())
+        ld.append(pairwise_LD(t, pop, tables, samples, idmap))
+        print(ld[-1])
+        genome_scan.extend(genome_scan_stats(
+            t, pop.mutations, tables, idmap, amd, sample_indexes_at_time))
 
-        # Get a random subset of the ancient samples
-        # for our genome scan
-        random_sample = np.random.choice(
-            sample_indexes_at_time, args.nsam, replace=False)
-        # Get the nodes associated w/those random samples
-        samples = amd['nodes'][random_sample].flatten()
-
-        # Simplify the tables w.r.to the samples.
-        # Simplification results in a much smaller
-        # set of trees to iterate over.
-        tables, idmap = fwdpy11.ts.simplify(pop, samples)
-
-        # These are the new indexes of our sample, after
-        # simplification
-        remapped = idmap[samples]
-
-        # Now, we build a numpy array of the genotypes
-        # containing both selected an neutral variants.
-        # Because we use np.concatenate, to join
-        # the neutral and selected sites, we have to be careful
-        # to create windows where the sites are in the correct
-        # order, which is increasing by position.  To
-        # accomplish this, we rely on np.argsort
-        gm = fwdpy11.ts.data_matrix_from_tables(
-            tables, pop.mutations, remapped, True, True)
-
-        pos = np.array(gm.neutral.positions + gm.selected.positions)
-        sorted_pos_indexes = np.argsort(pos)
-        all_sites = np.concatenate(
-            (np.array(gm.neutral, copy=False), np.array(gm.selected, copy=False)))
-        # We no longer need this object, and it is big.
-        del gm
-        # reorder the arrays
-        pos = pos[sorted_pos_indexes]
-        all_sites = all_sites[sorted_pos_indexes, :]
-
-        # Now, we get a big libsequence object...
-        vm = vmatrix.VariantMatrix(all_sites, pos)
-        # ... and count the alleles
-        ac = vm.count_alleles()
-
-        # Delete object we no longer need
-        del all_sites
-        del vm
-        # Now, we create our windows and iterate over them
-        # via numpy "fancy" indexing methods:
-        for locus, start_stop in enumerate(LOCUS_BOUNDARIES):
-            for window, start in enumerate(range(*start_stop)):
-                sites_in_window = np.where((pos >= start) & (
-                    pos[sorted_pos_indexes] < start + 1.))[0]
-                acw = ac[sites_in_window]
-                pi = sstats.thetapi(acw)
-                tajd = sstats.tajd(acw)
-                hprime = sstats.hprime(acw, 0)
-                genome_scan.append(GenomeScanRecord(
-                    t, locus, window, pi, tajd, hprime))
-
-    return repid, genome_scan, qtraits
+    return repid, genome_scan, qtraits, ld
 
 
 if __name__ == "__main__":
@@ -220,14 +230,14 @@ if __name__ == "__main__":
 
     used_seeds.clear()
 
-    for i in [args.gscan_dbfile, args.qtrait_dbfile]:
+    for i in [args.gscan_dbfile, args.qtrait_dbfile, args.ld_dbfile]:
         if os.path.exists(i):
             raise RuntimeError("File {} exists!".format(i))
 
     with concurrent.futures.ProcessPoolExecutor() as executor:
         futures = {executor.submit(process_replicate, i) for i in inputs}
         for fut in concurrent.futures.as_completed(futures):
-            repid, genome_scan, qtraits = fut.result()
+            repid, genome_scan, qtraits, ld = fut.result()
             df = pd.DataFrame(genome_scan, columns=GenomeScanRecord._fields)
             df['repid'] = [repid] * len(df.index)
             with sqlite3.connect(args.gscan_dbfile) as conn:
@@ -237,3 +247,8 @@ if __name__ == "__main__":
             df['repid'] = [repid] * len(df.index)
             with sqlite3.connect(args.qtrait_dbfile) as conn:
                 df.to_sql('data', conn, if_exists='append', index=False)
+            df = pd.DataFrame(ld, columns=LDRecord._fields)
+            df['repid'] = [repid] * len(df.index)
+            with sqlite3.connect(args.ld_dbfile) as conn:
+                df.to_sql('data', conn, if_exists='append', index=False)
+
