ac2cc88b7479c37c6bbcea89e15519fe28228c15
molpopgen <krthornt@uci.edu>
2019-04-30 14:39:59 -0700

    table -> fix + fixes

--- trajectories/fixation_table.R
+++ trajectories/fixation_table.R
@@ -1,9 +1,44 @@
 library(knitr)
 library(dplyr)
 library(readr)
+library(ggplot2)
 
 db=DBI::dbConnect(RSQLite::SQLite(),"all_fixation_summaries.sqlite3")
 dbt=tbl(db,"fixations")
+
+x = collect(dbt) %>%
+    mutate(scaled = 5e3*esize*esize,
+           Category = ifelse(origin < 5e4,"Standing variation",
+                             ifelse(origin<=5e4+100,"New mutation","None"))) %>%
+    filter(Category != "None", scaled <= 1e3) %>%
+    group_by(opt,mu,repid,strength=cut(scaled,c(0, 10, 100, 1000, 1e8)),Category) %>%
+    mutate(n=n()) %>%
+    group_by(opt, mu, strength, Category) %>%
+    summarise(mean_sweeps = mean(n),msojourn = mean(sojourn_time)/5e3)
+
+xlabels=c("(0,10]","(10,100]","(100,1000]",">= 1000")
+
+number = ggplot(x, aes(x=strength,y=mean_sweeps)) +
+    facet_grid(opt ~ mu,
+               labeller=label_bquote(rows=z[o]~"="~.(opt),cols=mu~"="~.(mu))) +
+    geom_point(alpha=0.5,aes(color=Category)) +
+    xlab(expression(paste('N',gamma^2,' range'))) +
+    ylab("Mean number of fixations") +
+    scale_x_discrete(labels=xlabels) + theme_bw() +
+    theme(axis.text.x = element_text(angle=45,hjust=1)) #+
+ggsave('NumFixationsSingleRegion.pdf')
+
+
+number = ggplot(x, aes(x=strength,y=msojourn)) +
+    facet_grid(opt ~ mu,
+               labeller=label_bquote(rows=z[o]~"="~.(opt),cols=mu~"="~.(mu))) +
+    geom_point(alpha=0.5,aes(color=Category)) +
+    xlab(expression(paste('N',gamma^2,' range'))) +
+    ylab("Mean fixation time (units of N generations)") +
+    scale_x_discrete(labels=xlabels) + theme_bw() +
+    theme(axis.text.x = element_text(angle=45,hjust=1)) #+
+ggsave('FixationTimesSingleRegion.pdf')
+q('no')
 
 soft=collect(dbt) %>%
     mutate(large = ifelse(abs(esize) >= 2*sqrt(2)*sqrt(mu),T,F)) %>%
