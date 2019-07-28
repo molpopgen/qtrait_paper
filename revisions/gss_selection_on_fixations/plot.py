0ff1b13b381bbe3a1484aa6b10ddec600671a9a8
molpopgen <krthornt@uci.edu>
2019-07-28 15:10:25 -0700

    Fix line widths, colors, legends, and add inset.

--- revisions/gss_selection_on_fixations/plot.py
+++ revisions/gss_selection_on_fixations/plot.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 
 TWON = 1e4
 TIME_OFFSET = 5e4
@@ -21,6 +22,7 @@ gs = gridspec.GridSpec(3, 3)
 freqaxes = []
 distanceaxes = []
 saxes = []
+saxes_inset = []
 
 freqaxes.append(plt.subplot(gs[0, 0]))
 freqaxes.append(plt.subplot(gs[0, 1], sharex=freqaxes[0], sharey=freqaxes[0]))
@@ -36,6 +38,12 @@ saxes.append(plt.subplot(gs[2, 0]))
 saxes.append(plt.subplot(gs[2, 1], sharex=saxes[0], sharey=saxes[0]))
 saxes.append(plt.subplot(gs[2, 2], sharex=saxes[0], sharey=saxes[0]))
 
+for i in range(len(saxes)):
+    saxes_inset.append(inset_axes(saxes[i],
+                                  width="50%",
+                                  height=0.25,
+                                  loc='upper right'))
+
 for i in range(len(datasets)):
     grps = datasets[i].groupby('position')
     # Get the time when the replicate first reaches 0.9*z_o and 0.975*z_o
@@ -43,23 +51,41 @@ for i in range(len(datasets)):
     idx = np.where(first_group[1].Gbar >= 0.9)[0]
     tadapted = idx[0]
     for g in grps:
-        xdata = g[1].generation-TIME_OFFSET
-        freqaxes[i].plot(xdata, g[1].nsamples/TWON)
-        distance = (g[1].G-1.0)  # /np.sqrt(g[1].VG)
-        distanceaxes[i].plot(xdata, distance)
-        s = ((g[1].G - 1.0)**2)/2  # VS = 1
-        esize = g[1].esize.unique()[0]
         ftime = int(g[1].ftime.unique()[0] - TIME_OFFSET)
         origin = int(g[1].origin.unique()[0] - TIME_OFFSET)
-        saxes[i].plot(xdata, s, label=r'$\gamma$'+f"={esize:.3}, {origin}, {ftime}")
+        esize = g[1].esize.unique()[0]
+        xdata = g[1].generation-TIME_OFFSET
+        label = r'$\gamma$'+f"={esize:.3}, {origin}, {ftime}"
+        freqaxes[i].plot(xdata, g[1].nsamples/TWON, label=label,
+                         linewidth=1)
 
-        freqaxes[i].axvline(tadapted, color="black", ls="dashed")
-        distanceaxes[i].axvline(tadapted, color="black", ls="dashed")
-        saxes[i].axvline(tadapted, color="black", ls="dashed")
-        saxes[i].axhline(2./TWON, color="black", ls="dotted")
+        distance = (g[1].G-1.0)  # /np.sqrt(g[1].VG)
+        distanceaxes[i].plot(xdata, distance, linewidth=1)
+        s = ((g[1].G - 1.0)**2)/2  # VS = 1
+        saxes[i].plot(xdata, s, linewidth=1)
+        saxes_inset[i].plot(xdata, s, linewidth=1)
 
-for axes in saxes:
-    axes.legend(loc='upper right', prop={'size': 6})
+        freqaxes[i].axvline(tadapted, color="grey", ls="dashed", linewidth=1)
+        distanceaxes[i].axvline(tadapted, color="grey",
+                                ls="dashed", linewidth=1)
+        saxes[i].axvline(tadapted, color="grey", ls="dashed", linewidth=1)
+        saxes[i].axhline(2./TWON, color="grey", ls="dotted", linewidth=1)
+        saxes_inset[i].axvline(tadapted, color="grey",
+                               ls="dashed", linewidth=1)
+        saxes_inset[i].axhline(2./TWON, color="grey", ls="dotted", linewidth=1)
+
+for axes in freqaxes[:2]:
+    axes.legend(loc='lower right', prop={'size': 5},
+                frameon=False)
+freqaxes[-1].legend(loc='upper right', prop={'size': 5},
+                    frameon=False)
+
+for axes in saxes_inset:
+    # axes.set_xlim(saxes[0].get_xlim()[0],75)
+    axes.set_xlim(20, 125)
+    axes.set_ylim(0.5*saxes[0].get_ylim()[0], 200./TWON)
+    for item in axes.get_xticklabels() + axes.get_yticklabels():
+        item.set_fontsize(6)
 
 # Hide some axes!!!
 for axes in freqaxes + distanceaxes:
@@ -76,5 +102,5 @@ freqaxes[0].set_ylabel("Frequency")
 distanceaxes[0].set_ylim(-1, 0.3)
 saxes[0].set_ylim(saxes[0].get_ylim()[0], 0.4)
 
-fig.align_ylabels()
+fig.align_ylabels([i[0] for i in [freqaxes, distanceaxes, saxes]])
 plt.savefig("StrengthOfSelection.pdf")
