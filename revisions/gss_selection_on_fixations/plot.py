fe8659373ed064dcac825766e787ba621dea3bd7
molpopgen <krthornt@uci.edu>
2019-08-01 10:55:02 -0700

    revise new fig

--- revisions/gss_selection_on_fixations/plot.py
+++ revisions/gss_selection_on_fixations/plot.py
@@ -1,13 +1,17 @@
 import sqlite3
-import glob
 import pandas as pd
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 
+matplotlib.rcParams.update({'font.size': 22})
+
 TWON = 1e4
 TIME_OFFSET = 5e4
+ALPHA = 0.6
+LINEWIDTH = 3
 
 
 datasets = []
@@ -16,7 +20,7 @@ for i in ['pop.sqlite3', 'pop2.sqlite3', 'pop3.sqlite3']:
         df = pd.read_sql("select * from data", conn)
         datasets.append(df)
 
-fig = plt.figure()
+fig = plt.figure(figsize=(15, 15))
 gs = gridspec.GridSpec(3, 3)
 
 freqaxes = []
@@ -41,7 +45,7 @@ saxes.append(plt.subplot(gs[2, 2], sharex=saxes[0], sharey=saxes[0]))
 for i in range(len(saxes)):
     saxes_inset.append(inset_axes(saxes[i],
                                   width="50%",
-                                  height=0.25,
+                                  height=0.5,
                                   loc='upper right'))
 
 for i in range(len(datasets)):
@@ -54,41 +58,51 @@ for i in range(len(datasets)):
         ftime = int(g[1].ftime.unique()[0] - TIME_OFFSET)
         origin = int(g[1].origin.unique()[0] - TIME_OFFSET)
         esize = g[1].esize.unique()[0]
-        if 5e3*esize*esize >= 100:
+        ls = '-'
+        if esize < 0:
+            ls = ':'
+        if 5e3*esize*esize >= 1:
             xdata = g[1].generation-TIME_OFFSET
-            label = r'$\gamma$'+f"={esize:.3}, {origin}, {ftime}"
+            Ng2 = 0.5*TWON*esize*esize
+            label = r'$N\gamma^2$'+f"={Ng2:.3}, {origin}, {ftime}"
             freqaxes[i].plot(xdata, g[1].nsamples/TWON, label=label,
-                             linewidth=1)
+                             linewidth=LINEWIDTH, ls=ls, alpha=ALPHA)
 
             distance = (g[1].G-1.0)  # /np.sqrt(g[1].VG)
-            distance = (g[1].G-g[1].Gbar) /np.sqrt(g[1].VG)
-            distanceaxes[i].plot(xdata, distance, linewidth=1)
+            distance = (g[1].G-g[1].Gbar) / np.sqrt(g[1].VG)
+            distanceaxes[i].plot(
+                xdata, distance, linewidth=LINEWIDTH, ls=ls, alpha=ALPHA)
             s = (g[1].W - g[1].Wbar)/g[1].Wbar  # VS = 1
-            saxes[i].plot(xdata, s, linewidth=1)
-            saxes_inset[i].plot(xdata, s, linewidth=1)
+            saxes[i].plot(xdata, s, linewidth=LINEWIDTH, ls=ls, alpha=ALPHA)
+            saxes_inset[i].plot(
+                xdata, s, linewidth=LINEWIDTH, ls=ls, alpha=ALPHA)
 
-            freqaxes[i].axvline(tadapted, color="grey", ls="dashed", linewidth=1)
+            freqaxes[i].axvline(tadapted, color="grey",
+                                ls="dashed", linewidth=LINEWIDTH)
             distanceaxes[i].axvline(tadapted, color="grey",
-                                    ls="dashed", linewidth=1)
-            saxes[i].axvline(tadapted, color="grey", ls="dashed", linewidth=1)
-            saxes[i].axhline(2./TWON, color="grey", ls="dotted", linewidth=1)
+                                    ls="dashed", linewidth=LINEWIDTH)
+            saxes[i].axvline(tadapted, color="grey",
+                             ls="dashed", linewidth=LINEWIDTH)
+            saxes[i].axhline(2./TWON, color="grey",
+                             ls="dotted", linewidth=LINEWIDTH)
             saxes_inset[i].axvline(tadapted, color="grey",
-                                   ls="dashed", linewidth=1)
-            saxes_inset[i].axhline(2./TWON, color="grey", ls="dotted", linewidth=1)
+                                   ls="dashed", linewidth=LINEWIDTH)
+            saxes_inset[i].axhline(2./TWON, color="grey",
+                                   ls="dotted", linewidth=LINEWIDTH)
 
 for axes in freqaxes[:1]:
-    axes.legend(loc='lower right', prop={'size': 5},
+    axes.legend(loc=(0.05, 0.67), prop={'size': 11},
                 frameon=False)
 for axes in freqaxes[1:]:
-        axes.legend(loc='upper center', prop={'size': 5},
-                    frameon=False)
+    axes.legend(loc='lower right', prop={'size': 11},
+                frameon=False)
 
 for axes in saxes_inset:
     # axes.set_xlim(saxes[0].get_xlim()[0],75)
     axes.set_xlim(20, 125)
     axes.set_ylim(0.5*saxes[0].get_ylim()[0], 500./TWON)
     for item in axes.get_xticklabels() + axes.get_yticklabels():
-        item.set_fontsize(6)
+        item.set_fontsize(11)
     axes.xaxis.set_tick_params(width=0.25)
     axes.yaxis.set_tick_params(width=0.25)
     for loc in ['top', 'bottom', 'left', 'right']:
