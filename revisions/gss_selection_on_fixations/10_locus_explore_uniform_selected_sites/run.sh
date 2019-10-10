82c59d3d871fd89a399293ebda931fb3cba65c42
molpopgen <krthornt@uci.edu>
2019-10-10 13:20:00 -0700

    new files

---
+++ revisions/gss_selection_on_fixations/10_locus_explore_uniform_selected_sites/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+parallel python3 ../simulate_10loci.py --popsize 5000 --VS 1 --mu 0.005 --filename himu.{}.gz --seed {} :::: himu
+parallel python3 ../simulate_10loci.py --popsize 5000 --VS 1 --mu 0.001 --filename midmu.{}.gz --seed {} :::: midmu
+parallel python3 ../simulate_10loci.py --popsize 5000 --VS 1 --mu 0.00025 --filename lowmu.{}.gz --seed {} :::: lowmu
+
+# ls -1 himu.*.gz midmu.*.gz lowmu.*.gz > infiles
+# cat infiles | sed 's/gz/sqlite3/' > outfiles
+# parallel --xapply python3 ../process_population.py --infile {1} --outfile {2} :::: infiles :::: outfiles
+# ls -1 himu.*.gz > himufiles.txt
+# ls -1 midmu.*.gz > midmufiles.txt
+# ls -1 lowmu.*.gz > lowmufiles.txt
+# 
+# for i in himufiles.txt midmufiles.txt lowmufiles.txt
+# do
+#     n=`basename $i .txt`
+#     cat $i | sed 's/gz/sqlite3/' > $n.outfiles.txt
+#     parallel --xapply python3 ../process_population.py --infile {1} --outfile {2} :::: $i :::: $n.outfiles.txt
+# done
